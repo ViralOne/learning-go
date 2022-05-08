62c1b2ca4eb0725d11ca25373677e48f6ecc99bf
Mocanu Marian <viralnico@protonmail.com>
2022-05-08 16:57:58 +0300

    Fix test naming

--- projects/hello_test.go
+++ projects/hello_test.go
@@ -4,11 +4,23 @@ import (
 	"testing"
 )
 
-func testH(t *testing.T) {
-	got := Hello()
-	want := "Hello, world."
+func TestHello(t *testing.T) {
+	t.Run("testHello", func(t *testing.T) {
+		got := Hello("Milu")
+		want := "Hello, Milu"
 
-	if got != want {
-		t.Errorf("Hello() = %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("Hello() = %q, want %q", got, want)
+		}
+	})
+
+	// failing test
+	t.Run("Test call empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, there"
+
+		if got != want {
+			t.Errorf("Hello() = %q, want %q", got, want)
+		}
+	})
 }
